200a31b3fe78472bc0093f0c8c611eaa1c526f81
aakash-sharma25 <as024harma@gmail.com>
2025-01-13 17:55:48 +0530

    added cors

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://assignment-frontend-gamma-six.vercel.app/",
+    ],
+    credentials: true, // Allow cookies and credentials
+  })
+);
 
 app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
